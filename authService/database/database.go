12d320eacc21bb9ab3c44e59bb2d8f18f5cd5658
dogayaglicioglu <doga_yaglicioglu@hotmail.com>
2024-07-16 23:46:21 +0300

    webdb not connected

--- authService/database/database.go
+++ authService/database/database.go
@@ -19,11 +19,12 @@ var DB DbInstance
 
 func ConnectDb() DbInstance {
 	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		"host=dbauth user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
 	)
+	fmt.Printf("DSN: %s\n", dsn)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
